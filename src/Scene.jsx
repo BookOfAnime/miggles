e63be96e63507d47485055ef2684b2b687dc93b4
Ephriam Kassa <ephriam123@gmail.com>
2024-07-20 11:14:46 -0700

    qqq

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,16 +1,35 @@
 import React, { Suspense, useRef, useEffect } from 'react';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import { Environment, Center, Text3D, useMatcapTexture, PerspectiveCamera, PresentationControls, OrbitControls } from '@react-three/drei';
+import { Environment, Text3D, useMatcapTexture, PerspectiveCamera, OrbitControls, useGLTF } from '@react-three/drei';
 import { useSpring, animated } from '@react-spring/three';
 import * as THREE from 'three';
 import gsap from 'gsap';
 
 import { Miggles } from './Miggles';  // Ensure this path is correct
 
+function Pyramid() {
+  const { scene } = useGLTF('/Pyramid.glb');
+  return <primitive object={scene} position={[0, -50, -100]} />;
+}
+
+function Cat() {
+  const { scene } = useGLTF('/cat.glb');
+  return <primitive scale={100} object={scene} position={[-10, 100, -75]} />;
+}
+
+function Floor() {
+  return (
+    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -60, -100]}>
+      <planeGeometry args={[1000000, 100000]} />
+      <meshStandardMaterial color="tan" />
+    </mesh>
+  );
+}
+
 function AnimatedText3D() {
   const textRef = useRef();
   const { viewport, camera } = useThree();
-  const [matcapTexture] = useMatcapTexture('3E2335_D36A1B_8E4A2E_2842A5', 256)
+  const [matcapTexture] = useMatcapTexture('3E2335_D36A1B_8E4A2E_2842A5', 256);
   const [springs, api] = useSpring(() => ({
     scale: [1, 1, 1],
     config: { mass: 2, tension: 500, friction: 40 }
@@ -33,12 +52,12 @@ function AnimatedText3D() {
   });
 
   return (
-    <Center position={[0, 50, 0]}>
+    <group position={[0, -150, 0]}>
       <animated.group scale={springs.scale}>
         <Text3D
           ref={textRef}
           font="/little.json"
-          size={viewport.width / 15}
+          size={viewport.width / 25}
           height={0.2}
           curveSegments={24}
           bevelEnabled
@@ -47,11 +66,11 @@ function AnimatedText3D() {
           bevelOffset={0}
           bevelSegments={5}
         >
-          {`3d\nMiggles`}
+          {`3d Miggles`}
           <meshStandardMaterial color="white" metalness={0.5} roughness={0.1} />
         </Text3D>
       </animated.group>
-    </Center>
+    </group>
   );
 }
 
@@ -161,27 +180,17 @@ export default function Scene() {
   return (
     <div style={{ position: 'relative', width: '100%', height: '100vh' }}>
       <Canvas>
-        <PerspectiveCamera makeDefault position={[0, 50, 200]} fov={50} />
+        <PerspectiveCamera makeDefault position={[0, 100, 600]} fov={50} />
         <OrbitControls />
-        {/* <PresentationControls
-          global
-          zoom={0.4}
-          rotation={[0, -Math.PI / 4, 0]}
-          polar={[0, Math.PI / 4]} // Restrict vertical rotation
-          azimuth={[-Math.PI / 4, Math.PI / 4]} // Restrict horizontal rotation
-          config={{ mass: 2, tension: 400 }} // Makes movement more smooth
-          snap={{ mass: 4, tension: 400 }} // Snapping back to center
-          minZoom={0.5} // Minimum zoom level
-          maxZoom={1.5} // Maximum zoom level
-        > */}
-          <Suspense fallback={null}>
-            <Miggles position={[0, -50, 0]} scale={1} />
-            <AnimatedText3D />
-            <Environment files="/desert.exr" background blur={0.5} />
-          </Suspense>
-        {/* </PresentationControls> */}
+        <Suspense fallback={null}>
+          <Pyramid />
+          <Cat />
+          <AnimatedText3D />
+        </Suspense>
         <ambientLight intensity={0.5} />
+        <Environment files={'./desert.exr'} background />
         <spotLight position={[100, 100, 100]} angle={0.15} penumbra={1} intensity={1} castShadow />
+        <pointLight position={[-100, -100, -100]} intensity={0.5} />
       </Canvas>
       <Navbar />
       <Footer />
